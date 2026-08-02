c37e235eb62a2348de85cd6e92b0b5c1a10dd20f
Pedro Cavallazzi <pedrocavallazzi@gmail.com>
2026-08-02 16:18:31 -0300

    feat: static file serving and centralized error handler

--- packages/api/src/app.js
+++ packages/api/src/app.js
@@ -1,10 +1,14 @@
 'use strict'
 require('dotenv').config()
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const templatesRouter = require('./routes/templates')
 const dataRouter = require('./routes/data')
 const renderRouter = require('./routes/render')
+const errorHandler = require('./middleware/errorHandler')
+
+const BUILDER_DIST = path.join(__dirname, '../../builder/dist')
 
 function createApp() {
   const app = express()
@@ -15,6 +19,11 @@ function createApp() {
   app.use('/api/templates', templatesRouter)
   app.use('/api/data', dataRouter)
   app.use('/api/render', renderRouter)
+
+  app.use(express.static(BUILDER_DIST))
+  app.get('*', (req, res) => res.sendFile(path.join(BUILDER_DIST, 'index.html')))
+
+  app.use(errorHandler)
 
   return app
 }
